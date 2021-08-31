22b1b3500c9da72cc05478d666bc171338b7b152
AmadeoMz <88296806+AmadeoMz@users.noreply.github.com>
2021-08-31 14:20:45 -0500

    Create rows_columns_camparison.py

---
+++ rows_columns_camparison.py
@@ -0,0 +1,29 @@
+import sys
+import numpy as np
+
+def main():
+	"""This program open the .csv files specified in the command-line with the NumPy library 
+	and compares the number of rows and columns from each file.
+	"""
+
+	script=sys.argv[0]
+	filenames = sys.argv[1:]
+	assert len(filenames)>=2, 'Please introduce at least two filenames'
+	check(filenames)
+
+
+
+def check(filenames):
+	for file in filenames:
+	 data=np.loadtxt(fname=file, delimiter=',')
+	 (rows, columns) = data.shape
+	 for file_comp in filenames:
+	   data_comp=np.loadtxt(fname=file_comp, delimiter=',')
+	   (rows_comp, columns_comp) = data_comp.shape
+	   assert rows == rows_comp, 'At least one is different:'
+	   assert columns == columns_comp, 'At least one is different:'
+	print('All the files have the same number of rows and columns', (rows, columns))
+
+
+if __name__=='__main__':
+	main()
